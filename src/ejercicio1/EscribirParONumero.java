1be8259d8831a8bcfa58945e03719644d8f23007
Ramiro Ridolfi <ramiroenet35@gmail.com>
2024-05-20 17:45:36 -0300

    Agregando ejercicio 1

---
+++ src/ejercicio1/EscribirParONumero.java
@@ -0,0 +1,17 @@
+package ejercicio1;
+
+public class EscribirParONumero {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 100; i++) {
+			if (i %2 == 0) {
+				System.out.println("Par");
+			} else {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
